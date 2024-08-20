8cc6220ec3a158872a15d47b0a63d2488ba40733
rodrigoito <rodrigoito677@gmail.com>
2024-08-20 17:29:27 -0300

    100%

--- conta-banco/src/ContaBanco.java
+++ conta-banco/src/ContaBanco.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class ContaBanco {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o número da conta do cliente: ");
+        int numeroConta = scanner.nextInt();
+
+        // Consumir a linha restante (o caractere de nova linha deixado no buffer)
+        scanner.nextLine();
+
+        System.out.println("Digite a Agência do cliente: ");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Digite o nome do cliente: ");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Digite o saldo da conta do cliente: ");
+        double saldoCliente = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar a conta em nosso banco. Sua agência é: " + agencia + ", o número da sua conta é: " + numeroConta + ", e seu saldo é: " + saldoCliente);
+        
+        scanner.close();
     }
 }
